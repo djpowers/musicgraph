0c4f7c88c38a56b9cedd4b712cdff9458cd121b8
Dave Powers <djpowers89@gmail.com>
2015-02-26 00:00:12 -0500

    Implement album search by title

--- lib/musicgraph/album.rb
+++ lib/musicgraph/album.rb
@@ -1,7 +1,17 @@
 module MusicGraph
   class Album
+    attr_reader :title
+
+    API_URL = "http://api.musicgraph.com/api/v2/album/"
 
     def initialize(attributes)
+      @title = attributes["title"]
+    end
+
+    def self.search(params)
+      response = Faraday.get("#{API_URL}search?#{MusicGraph.key_param}&title=#{params}")
+      albums = JSON.parse(response.body)["data"]
+      albums.map { |attributes| new(attributes) }
     end
   end
 end
--- spec/musicgraph/album_spec.rb
+++ spec/musicgraph/album_spec.rb
@@ -4,5 +4,18 @@ module MusicGraph
     it "exists" do
       expect(MusicGraph::Album)
     end
+
+    describe "#search" do
+      it "returns album name search results" do
+        VCR.use_cassette("album", record: :new_episodes) do
+          album_name = "either/or"
+          albums = MusicGraph::Album.search(album_name)
+
+          expect(albums).to_not be_empty
+          expect(albums.first).to be_a Album
+          expect(albums.first.title).to eql("Either/Or")
+        end
+      end
+    end
   end
 end
